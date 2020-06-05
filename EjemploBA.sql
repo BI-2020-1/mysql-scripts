bf46846fceeb97c26e222bb68860b138a7471485
leayobana <leaquillx@gmail.com>
2020-06-04 22:43:56 -0500

    ejemploBA

---
+++ EjemploBA.sql
@@ -0,0 +1,40 @@
+
+SELECT 
+	DP.material,
+	sum(venta_monto) AS TOTAL_VENTAS
+	FROM H_VENTA AS HV
+	inner join DPRODUCTO AS DP ON HV.id_producto = DP.id_producto
+GROUP BY DP.material;
+
+SELECT 
+	DP.talla,
+	sum(venta_monto) AS TOTAL_VENTAS
+	FROM H_VENTA AS HV
+	inner join DPRODUCTO AS DP ON HV.id_producto = DP.id_producto
+GROUP BY DP.talla;
+
+SELECT 
+	DP.categoria_nombre,
+	sum(venta_monto) AS TOTAL_VENTAS
+	FROM H_VENTA AS HV
+	inner join DPRODUCTO AS DP ON HV.id_producto = DP.id_producto
+GROUP BY DP.categoria_nombre;
+ 
+SELECT 
+		sum(venta_monto) as Ventas
+FROM venta_dm.H_VENTA as HP;
+
+SELECT 
+		DT.turno_dia,
+		sum(venta_monto) as Ventas
+FROM venta_dm.H_VENTA as HV
+		inner join DTIEMPO AS DT ON HV.id_tiempo = DT.id_tiempo
+GROUP BY DT.turno_dia;
+    
+SELECT 
+		DT.dia_semana,
+		sum(cantidad_unidades_vendidas) as cantidad
+FROM venta_dm.H_VENTA as HV
+		inner join DTIEMPO AS DT ON HV.id_tiempo = DT.id_tiempo
+GROUP BY DT.dia_semana;
+    
